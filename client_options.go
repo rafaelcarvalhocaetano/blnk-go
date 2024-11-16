7c747234bc42b70044b26bbbda085159527be6c2
Abdulrahman Salau <bob.salau@gmail.com>
2024-11-16 16:12:50 +0100

    made options private so it can only be set via Option functions on initialization

--- client_options.go
+++ client_options.go
@@ -6,18 +6,18 @@ type ClientOption func(*Client)
 
 func WithLogger(logger Logger) ClientOption {
 	return func(c *Client) {
-		c.Options.Logger = logger
+		c.options.Logger = logger
 	}
 }
 
 func WithRetry(count int) ClientOption {
 	return func(c *Client) {
-		c.Options.RetryCount = count
+		c.options.RetryCount = count
 	}
 }
 
 func WithTimeout(timeout time.Duration) ClientOption {
 	return func(c *Client) {
-		c.Options.Timeout = timeout
+		c.options.Timeout = timeout
 	}
 }
