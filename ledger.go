760d51f75b1ac26977f3beb86bfaa66057b16569
Abdulrahman Salau <bob.salau@gmail.com>
2024-11-16 16:12:18 +0100

    feat(Ledger): added logic for ledger requests

---
+++ ledger.go
@@ -0,0 +1,52 @@
+package blnkgo
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type LedgerService service
+
+type Ledger struct {
+	LedgerID  string                 `json:"ledger_id"`
+	Name      string                 `json:"name"`
+	CreatedAt time.Time              `json:"created_at"`
+	MetaData  map[string]interface{} `json:"meta_data,omitempty"`
+}
+
+type CreateLedgerRequest struct {
+	Name     string                 `json:"name"`
+	MetaData map[string]interface{} `json:"meta_data,omitempty"`
+}
+
+func (s *LedgerService) Get(id string) (*Ledger, *http.Response, error) {
+	u := fmt.Sprintf("ledgers/%s", id)
+	req, err := s.client.NewRequest(u, "GET", nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ledger := new(Ledger)
+	resp, err := s.client.CallWithRetry(req, &ledger)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return ledger, resp, nil
+}
+
+func (s *LedgerService) Create(body *CreateLedgerRequest) (*Ledger, *http.Response, error) {
+	req, err := s.client.NewRequest("ledgers", "POST", body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ledger := new(Ledger)
+	resp, err := s.client.CallWithRetry(req, &ledger)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return ledger, resp, nil
+}
