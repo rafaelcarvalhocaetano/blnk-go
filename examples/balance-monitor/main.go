bfaf5490beaa97e90affe2cba5c7aaf3f2c18981
Abdulrahman Salau <bob.salau@gmail.com>
2024-11-28 13:32:04 +0100

    example(Balance-Monitor): added example for balance monitor

---
+++ examples/balance-monitor/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+
+	blnkgo "github.com/blnkfinance/blnk-go"
+)
+
+func main() {
+	baseURL, _ := url.Parse("http://localhost:5001/")
+	client := blnkgo.NewClient(baseURL, nil)
+
+	ledgerBody := blnkgo.CreateLedgerRequest{
+		Name: "Ledge",
+		MetaData: map[string]interface{}{
+			"project_name": "SendWorldApp",
+		},
+	}
+
+	ledger, resp, err := client.Ledger.Create(ledgerBody)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(ledger.LedgerID)
+
+	//create a balance
+	ledgerBalanceBody := blnkgo.CreateLedgerBalanceRequest{
+		LedgerID: ledger.LedgerID,
+		Currency: "USD",
+		MetaData: map[string]interface{}{
+			"customer_name": "SendWorldApp",
+		},
+	}
+
+	ledgerBalance, resp, err := client.LedgerBalance.Create(ledgerBalanceBody)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	fmt.Println(resp.StatusCode)
+	fmt.Println(ledgerBalance.BalanceID)
+
+	//create a balance monitor
+	ledgerBalanceMonitorBody := blnkgo.MonitorData{
+		BalanceID: ledgerBalance.BalanceID,
+		Condition: blnkgo.MonitorCondition{
+			Field:     "credit_balance",
+			Operator:  blnkgo.OperatorGreaterThan,
+			Value:     1000,
+			Precision: 100,
+		},
+	}
+
+	bl, resp, err := client.BalanceMonitor.Create(ledgerBalanceMonitorBody)
+
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(bl.MonitorID)
+}
